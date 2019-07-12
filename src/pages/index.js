6ec42237ea79e509cc60ffd37e51bcfcd3439064
Sophie Au <SophieAu@user.noreply.github.com>
2019-07-12 22:22:56 +0200

    Add Info section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Hero from "../sections/home/Hero";
+import Info from "../sections/home/Info";
 
 export default () => (
   <Layout>
     <Hero />
+    <Info />
   </Layout>
 );
---
+++ src/sections/home/Info.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Info.scss";
+
+const InfoBoxes = [
+  {
+    image: "/img/info_blackwatch.jpg",
+    text: `Fight in the Revolutionary Wars. Command your armies to destroy the old order or preserve it. Rewrite history. The world is at your feet. To give you an authentic feeling for the battles of this period we created armies with historically accurate uniforms.`
+  },
+  {
+    image: "/img/info_portraits.jpg",
+    text: `But historical accuracy does not end with new fancy uniforms. There are many historical characters in the game that you already know or whose portrait you might have seen in a museum. More than 230 portraits have been added to the game to replace the vanilla portrait sets of ETW and give you an 18th century feeling.`
+  },
+  {
+    image: "/img/info_gameplay.jpg",
+    text:
+      "Additionally, RotR gives a more authentic feeling on the campaign map. Several factions have a new UI and all have their historical flags. You might also recognize some of the cities and buildings as 3D recreations of their actual historical counterparts."
+  }
+];
+
+export default () => (
+  <section class="info-boxes">
+    {InfoBoxes.map((box, i) =>
+      i % 2 ? (
+        <>
+          <img className="info-image" src={box.image} alt="" />
+          <div class="info-text">{box.text}</div>
+        </>
+      ) : (
+        <>
+          <div class="info-text">{box.text}</div>
+          <img className="info-image" src={box.image} alt="" />
+        </>
+      )
+    )}
+  </section>
+);
