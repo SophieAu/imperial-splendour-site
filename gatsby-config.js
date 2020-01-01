229dd2bfd4b360f569a2caf208a8f98f9524f969
Sophie Au <SophieAu@users.noreply.github.com>
2020-01-01 15:07:01 +0100

    Fix gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,18 @@
+/* eslint-disable @typescript-eslint/camelcase */
 module.exports = {
   plugins: [
     `gatsby-plugin-typescript`,
     `gatsby-plugin-feed`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
-    "gatsby-plugin-sharp",
+    'gatsby-plugin-sharp',
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog-posts`,
-        path: `${__dirname}/src/pages/blog-posts/`
-      }
+        path: `${__dirname}/src/pages/blog-posts/`,
+      },
     },
     {
       resolve: `gatsby-transformer-remark`,
@@ -18,13 +20,11 @@ module.exports = {
         plugins: [
           `gatsby-remark-prismjs`,
           {
-            resolve: "gatsby-remark-images",
-            options: {
-              maxWidth: 970
-            }
-          }
-        ]
-      }
+            resolve: 'gatsby-remark-images',
+            options: { maxWidth: 970 },
+          },
+        ],
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -35,24 +35,25 @@ module.exports = {
         background_color: `#B19776`,
         theme_color: `#B19776`,
         display: `minimal-ui`,
-        icon: `favicon.png` // This path is relative to the root of the site.
-      }
+        icon: `favicon.png`, // This path is relative to the root of the site.
+      },
     },
     {
       resolve: `gatsby-plugin-csp`,
       options: {
         disableOnDev: true,
+        mergeStyleHashes: false,
         directives: {
-          "default-src": "'none'",
-          "img-src": "'self' https://*",
-          "worker-src": "'none'",
-          "frame-src":
+          'default-src': "'none'",
+          'img-src': "'self' https://*",
+          'worker-src': "'none'",
+          'frame-src':
             "'self' https://sketchfab.com/models/ https://www.youtube-nocookie.com/embed/",
-          "script-src": "'self' https://static.sketchfab.com/static/",
-          "style-src": "'self' https://fonts.googleapis.com/css",
-          "font-src": "'self' https://fonts.gstatic.com/s/"
-        }
-      }
-    }
-  ]
+          'script-src': "'self' https://static.sketchfab.com/static/",
+          'style-src': "'self' 'unsafe-inline' blob:https://fonts.googleapis.com/css",
+          'font-src': "'self' https://fonts.gstatic.com/s/",
+        },
+      },
+    },
+  ],
 };
