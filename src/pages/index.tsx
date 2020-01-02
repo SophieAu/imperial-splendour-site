df777ff0677f00a79e3bc105e6d867423bd0ab0c
Sophie Au <SophieAu@users.noreply.github.com>
2020-01-02 12:04:38 +0100

    Finish Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,37 +7,30 @@ import Layout from '../components/Layout';
 import DownloadButton from '../components/ui/DownloadButton';
 import { slugs } from '../config';
 import { home } from '../strings';
+import { InfoBox } from '../types';
 
 const Home = () => (
   <Layout title={home.pageTitle} description={home.pageDescription} slug={slugs.home}>
-    <Hero />
-    <Info />
+    <section className="hero">
+      <div className="hero-body">
+        <img className="hero-logo" src={heroLogo} alt={home.heroLogoAlt} />
+        <p className="hero-text">{home.heroText}</p>
+      </div>
+      <DownloadButton linkTo={slugs.downloadIndex} className="hero-btn" />
+    </section>
+    <section className="info-boxes">
+      {home.infoBoxes.map((box, i) => (
+        <InfoBox key={i} box={box} i={i} />
+      ))}
+    </section>
   </Layout>
 );
 
-const Hero = () => (
-  <section className="hero">
-    <div className="hero-body">
-      <img className="hero-logo" src={heroLogo} alt={home.heroLogoAlt} />
-      <p className="hero-text">{home.heroText}</p>
-    </div>
-    <DownloadButton linkTo={slugs.downloadIndex} className="hero-btn" />
-  </section>
-);
-
-const Info = () => (
-  <section className="info-boxes">
-    {home.infoBoxes.map((box, i) => (
-      <>
-        <img
-          className={`info-image-${i % 2 ? 'even' : 'odd'}`}
-          src={box.image}
-          alt={box.imageAlt}
-        />
-        <div className="info-text">{box.text}</div>
-      </>
-    ))}
-  </section>
+const InfoBox: React.FC<{ box: InfoBox; i: number }> = ({ box, i }) => (
+  <>
+    <img className={`info-image-${i % 2 ? 'even' : 'odd'}`} src={box.image} alt={box.imageAlt} />
+    <p className="info-text">{box.text}</p>
+  </>
 );
 
 export default Home;
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type InfoBox = {
+  image: string;
+  imageAlt: string;
+  text: string;
+};
