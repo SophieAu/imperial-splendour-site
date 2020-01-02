41a41b3b4041d351ea4d2161eb09a31178f45663
Sophie Au <SophieAu@users.noreply.github.com>
2020-01-02 12:43:07 +0100

    Fix slug/path on Home screen

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,18 @@ export const TWITTER_HANDLE = '@SplendourTeam';
 
 export const slugs = {
   home: '',
-  downloadIndex: '/download',
-  downloadRotR: '/download/rotr',
+  downloadIndex: 'download',
+  downloadRotR: 'download/rotr',
+  blog: 'blog',
+  about: 'about',
+};
+
+export const paths = {
+  home: '/',
+  downloadIndex: `/${slugs.downloadIndex}`,
+  downloadRotR: `/${slugs.downloadRotR}`,
+  blog: `/${slugs.blog}`,
+  about: `/${slugs.about}`,
 };
 
 export const socialMedia = {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import heroLogo from '../../static/img/hero_logo.png';
 import Layout from '../components/Layout';
 import DownloadButton from '../components/ui/DownloadButton';
-import { slugs } from '../config';
+import { paths, slugs } from '../config';
 import { home } from '../strings';
 import { InfoBox } from '../types';
 
@@ -16,7 +16,7 @@ const Home = () => (
         <img className="hero-logo" src={heroLogo} alt={home.heroLogoAlt} />
         <p className="hero-text">{home.heroText}</p>
       </div>
-      <DownloadButton linkTo={slugs.downloadIndex} className="hero-btn" />
+      <DownloadButton linkTo={paths.downloadIndex} className="hero-btn" />
     </section>
     <section className="info-boxes">
       {home.infoBoxes.map((box, i) => (
