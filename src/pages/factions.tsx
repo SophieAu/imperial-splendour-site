8733e6051c226db03e7d5dc3ede10d36a609e3c5
Sophie Au <SophieAu@users.noreply.github.com>
2020-01-26 21:07:16 +0100

    Show ugly list of all factions on faction page

--- src/pages/factions.tsx
+++ src/pages/factions.tsx
@@ -1,18 +1,76 @@
 import './factions.scss';
 
+import { graphql } from 'gatsby';
+import Img, { FixedObject } from 'gatsby-image';
 import React from 'react';
 
 import { slugs } from '../../data/config';
 import { factions } from '../../data/strings';
 import Layout from '../components/Layout';
+import Link from '../components/Link';
 
-const Factions = () => (
+export const query = graphql`
+  query {
+    allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/data/content/factions/" } }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            slug
+            flag {
+              childImageSharp {
+                fixed {
+                  ...GatsbyImageSharpFixed
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+interface Props {
+  data: {
+    allMarkdownRemark: {
+      edges: {
+        node: {
+          id: number;
+          frontmatter: {
+            title: string;
+            slug: string;
+            flag: {
+              childImageSharp: {
+                fixed: FixedObject;
+              };
+            };
+          };
+        };
+      }[];
+    };
+  };
+}
+
+const Factions: React.FC<Props> = ({ data }) => (
   <Layout
     title={factions.pageTitle({ title: 'Factions' })}
     description={factions.pageDescription}
     slug={slugs.factions}
   >
-    <section className="factions"></section>
+    <section className="factions">
+      <ul>
+        {data.allMarkdownRemark.edges.map(faction => (
+          <li key={faction.node.id}>
+            <Link to={`/factions/${faction.node.frontmatter.slug}`}>
+              <h2>{faction.node.frontmatter.title}</h2>
+            </Link>
+            <Img fixed={faction.node.frontmatter.flag.childImageSharp.fixed} />
+          </li>
+        ))}
+      </ul>
+    </section>
   </Layout>
 );
 
