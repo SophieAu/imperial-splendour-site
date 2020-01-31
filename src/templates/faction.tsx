4326513def9ed78a150ba2ee016849f602c5c0b8
Sophie Au <SophieAu@users.noreply.github.com>
2020-01-31 21:47:29 +0100

    Do ugly carousel with correct urls on individual faction pages

--- src/templates/faction.tsx
+++ src/templates/faction.tsx
@@ -1,10 +1,12 @@
 import './post.scss';
 
 import { graphql } from 'gatsby';
-import React from 'react';
+import { FixedObject } from 'gatsby-image';
+import React, { useEffect, useState } from 'react';
 
-import { factions } from '../../data/strings';
+import { factions as factionStrings } from '../../data/strings';
 import Layout from '../components/Layout';
+import Carousel from '../components/ui/Carousel';
 
 export const query = graphql`
   query($id: String!) {
@@ -13,6 +15,25 @@ export const query = graphql`
       html
       frontmatter {
         title
+      }
+    }
+    allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/data/content/factions/" } }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            slug
+            flag {
+              childImageSharp {
+                fixed(height: 66, quality: 90) {
+                  ...GatsbyImageSharpFixed_withWebp
+                }
+              }
+            }
+          }
+          html
+        }
       }
     }
   }
@@ -29,20 +50,60 @@ interface Props {
       };
       html: string;
     };
+    allMarkdownRemark: {
+      edges: {
+        node: {
+          id: number;
+          frontmatter: {
+            title: string;
+            slug: string;
+            flag: {
+              childImageSharp: {
+                fixed: FixedObject;
+              };
+            };
+          };
+          html: string;
+        };
+      }[];
+    };
   };
 }
 
-const Faction: React.FC<Props> = ({ data: { markdownRemark: pageData }, pageContext }) => (
-  <Layout
-    title={factions.pageTitle({ title: pageData.frontmatter.title })}
-    description={pageData.frontmatter.title}
-    slug={pageContext.slug}
-  >
-    <article className="blog-post">
-      <h1>{pageData.frontmatter.title}</h1>
-      <div className="text" dangerouslySetInnerHTML={{ __html: pageData.html }} />
-    </article>
-  </Layout>
-);
+const Faction: React.FC<Props> = ({ data: { allMarkdownRemark, markdownRemark }, pageContext }) => {
+  const factions = allMarkdownRemark.edges;
+  const { html, frontmatter } = markdownRemark;
+
+  const current = factions.find(faction => faction.node.frontmatter.slug === pageContext.slug);
+  if (!current) return null;
+  const index = factions.indexOf(current);
+  if (index === -1) return null;
+
+  const [counter, setCounter] = useState(index);
+
+  useEffect(() => {
+    if (factions[counter].node.frontmatter.slug !== pageContext.slug) {
+      location.assign(`/factions/${factions[counter].node.frontmatter.slug}`);
+    }
+  }, [counter]);
+
+  return (
+    <Layout
+      title={factionStrings.pageTitle({ title: frontmatter.title })}
+      description={frontmatter.title}
+      slug={pageContext.slug}
+    >
+      <Carousel
+        img={factions.map(faction => faction.node.frontmatter.flag.childImageSharp.fixed)}
+        selected={counter}
+        onPress={(counter: number) => setCounter(counter)}
+      />
+      <article className="blog-post">
+        <h1>{frontmatter.title}</h1>
+        <div className="text" dangerouslySetInnerHTML={{ __html: html }} />
+      </article>
+    </Layout>
+  );
+};
 
 export default Faction;
