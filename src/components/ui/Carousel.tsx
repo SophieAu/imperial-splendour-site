01bd08e29794dc0603bfee9eb901c2f6f1416f31
Sophie Au <SophieAu@users.noreply.github.com>
2020-01-31 22:06:28 +0100

    Make flag loading less ugly

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -18,31 +18,66 @@ const Carousel: React.FC<Props> = ({ img, onPress, selected }) => {
   return (
     <div id="carousel">
       <div className="hideLeft">
-        <Img fixed={img[getModulo(selected - 3)]} className="img" />
+        <Img
+          fixed={img[getModulo(selected - 3)]}
+          className="img"
+          fadeIn={false}
+          placeholderStyle={{ display: 'none' }}
+        />
       </div>
       <div className="prevLeftSecond">
-        <Img fixed={img[getModulo(selected - 2)]} className="img" />
+        <Img
+          fixed={img[getModulo(selected - 2)]}
+          className="img"
+          fadeIn={false}
+          placeholderStyle={{ display: 'none' }}
+        />
       </div>
       <div className="prev">
-        <Img fixed={img[getModulo(selected - 1)]} className="img" />
+        <Img
+          fixed={img[getModulo(selected - 1)]}
+          className="img"
+          fadeIn={false}
+          placeholderStyle={{ display: 'none' }}
+        />
       </div>
       <button id="prev" onClick={() => onPress(getModulo(selected - 1))}>
         Prev
       </button>
       <div className="selected">
-        <Img fixed={img[getModulo(selected)]} className="img" />
+        <Img
+          fixed={img[getModulo(selected)]}
+          className="img"
+          fadeIn={false}
+          placeholderStyle={{ display: 'none' }}
+        />
       </div>
       <button id="next" onClick={() => onPress(getModulo(selected + 1))}>
         Next
       </button>
       <div className="next">
-        <Img fixed={img[getModulo(selected + 1)]} className="img" />
+        <Img
+          fixed={img[getModulo(selected + 1)]}
+          className="img"
+          fadeIn={false}
+          placeholderStyle={{ display: 'none' }}
+        />
       </div>
       <div className="nextRightSecond">
-        <Img fixed={img[getModulo(selected + 2)]} className="img" />
+        <Img
+          fixed={img[getModulo(selected + 2)]}
+          className="img"
+          fadeIn={false}
+          placeholderStyle={{ display: 'none' }}
+        />
       </div>
       <div className="hideRight">
-        <Img fixed={img[getModulo(selected + 3)]} className="img" />
+        <Img
+          fixed={img[getModulo(selected + 3)]}
+          className="img"
+          fadeIn={false}
+          placeholderStyle={{ display: 'none' }}
+        />
       </div>
     </div>
   );
