aac8911fe531f340e335650c8e019ae15d756a66
Sophie Au <SophieAu@users.noreply.github.com>
2020-02-02 20:30:18 +0100

    Clean up carousel

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -6,8 +6,13 @@ import React from 'react';
 import { FactionsFrontmatter } from '../../types';
 import Link from '../Link';
 
-const getBetterModulo = (base: number) => (value: number) =>
+const circularModulo = (base: number) => (value: number) =>
   value < 0 ? base + value : value % base;
+
+const FLAG_WIDTH = 132;
+const FLAG_HEIGHT = 66;
+const calcWidth = (offset: number) => FLAG_WIDTH / Math.abs(offset || 1);
+const calcHeight = (offset: number) => FLAG_HEIGHT * (1 - (offset * offset) / 100);
 
 interface Props {
   selected: number;
@@ -20,46 +25,37 @@ interface Props {
   }[];
 }
 
-const preSelected = [4, 3, 2, 1];
-
-const postSelected = [1, 2, 3, 4];
+const indices = [-4, -3, -2, -1, 0, 1, 2, 3, 4];
 
 const Carousel: React.FC<Props> = ({ factions, selected }) => {
-  const getModulo = getBetterModulo(factions.length);
+  const modulo = circularModulo(factions.length);
 
-  const onPress = (number: number) => `/factions/${factions[number].node.frontmatter.slug}`;
+  const onPress = (number: number) =>
+    `/factions/${factions[modulo(selected + number)].node.frontmatter.slug}`;
+
+  const imageProps = (index: number) => ({
+    src: factions[modulo(selected + index)].node.frontmatter.flag.childImageSharp.fixed,
+    title: factions[modulo(selected + index)].node.frontmatter.title,
+    offset: index,
+  });
 
   return (
     <div id="carousel">
-      {preSelected.map(index => (
-        <Image
-          key={factions[getModulo(selected - index)].node.id}
-          src={factions[getModulo(selected - index)].node.frontmatter.flag.childImageSharp.fixed}
-          title={factions[getModulo(selected - index)].node.frontmatter.title}
-          offset={-index}
-          side="left"
-        />
-      ))}
-      <Link className="link prev" to={onPress(getModulo(selected - 1))}>
-        {`<`}
-      </Link>
-      <Image
-        src={factions[getModulo(selected)].node.frontmatter.flag.childImageSharp.fixed}
-        title={factions[getModulo(selected)].node.frontmatter.title}
-        offset={0}
-      />
-      <Link className="link next" to={onPress(getModulo(selected + 1))}>
-        {`>`}
-      </Link>
-      {postSelected.map(index => (
-        <Image
-          key={factions[getModulo(selected + index)].node.id}
-          src={factions[getModulo(selected + index)].node.frontmatter.flag.childImageSharp.fixed}
-          title={factions[getModulo(selected + index)].node.frontmatter.title}
-          offset={index}
-          side="right"
-        />
-      ))}
+      {indices.map(index =>
+        index === 0 ? (
+          <>
+            <Link className="link" to={onPress(-1)}>
+              {`<`}
+            </Link>
+            <Image {...imageProps(0)} side="center" />
+            <Link className="link" to={onPress(1)}>
+              {`>`}
+            </Link>
+          </>
+        ) : (
+          <Image {...imageProps(index)} side={index > 0 ? 'right' : 'left'} />
+        )
+      )}
     </div>
   );
 };
@@ -68,13 +64,13 @@ const Image: React.FC<{
   src: FixedObject;
   offset: number;
   title: string;
-  side?: 'right' | 'left';
+  side: 'right' | 'left' | 'center';
 }> = ({ src, offset, title, side }) => (
   <Img
     fixed={src}
     style={{
-      height: `${(66 * (100 - offset * offset)) / 100}px`,
-      width: `${132 / Math.abs(offset || 1)}px`,
+      height: `${calcHeight(offset)}px`,
+      width: `${calcWidth(offset)}px`,
       zIndex: -Math.abs(offset),
       overflow: 'unset',
     }}
@@ -83,10 +79,10 @@ const Image: React.FC<{
     alt={title}
     imgStyle={{
       objectFit: 'cover',
-      objectPosition: `${side || 'center'} center`,
-      width: '132px',
-      right: `${side === 'left' ? 132 / Math.abs(offset || 1) - 132 : 0}px`,
-      left: `${side === 'right' ? 132 / Math.abs(offset || 1) - 132 : 0}px`,
+      objectPosition: `${side} center`,
+      width: `${FLAG_WIDTH}px`,
+      right: `${side === 'left' ? calcWidth(offset) - FLAG_WIDTH : 0}px`,
+      left: `${side === 'right' ? calcWidth(offset) - FLAG_WIDTH : 0}px`,
     }}
   />
 );
