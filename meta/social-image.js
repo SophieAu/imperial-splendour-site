bbf631576f524bf359d4ccd60a68df33e598cdc7
Sophie Au <SophieAu@users.noreply.github.com>
2020-02-14 14:11:54 +0100

    Dynamically generate author images

--- meta/social-image.js
+++ meta/social-image.js
@@ -6,8 +6,16 @@ const { resolve } = require('path');
 const { promisify } = require('util');
 const writeFileAsync = promisify(writeFile);
 
-const renderCard = (title, author) =>
-  `<head><link href="https://fonts.googleapis.com/css?family=IM+Fell+English&display=swap" rel="stylesheet" /><link href="https://fonts.googleapis.com/css?family=IM+Fell+English+SC&display=swap" rel="stylesheet" /></head><body style="margin:0"><div style="background-image: url(../../data/img/texture_bg.webp);width:1080px;height:510px;padding:60px;display: flex;flex-direction: column;justify-content: space-between;"><div style="display: flex;flex-direction: row;justify-content: space-between;"><img src="../../data/img/index/hero_logo.png" style="width:540px;"/><img src="../../data/img/about/${author}_portrait.png" style="width:150px;height:100%;"/></div><p style="font-family: IM Fell English;font-size:80px;height:230px">${title}</p></div></body>`;
+const renderCard = (title, authors = []) => {
+  const authorPortraits = authors
+    .map(
+      auth =>
+        `<img src="../../data/img/about/${auth}_portrait.png" style="padding-left:20px;width:150px;height:100%;"/>`
+    )
+    .join('');
+
+  return `<head><link href="https://fonts.googleapis.com/css?family=IM+Fell+English&display=swap" rel="stylesheet" /><link href="https://fonts.googleapis.com/css?family=IM+Fell+English+SC&display=swap" rel="stylesheet" /></head><body style="margin:0"><div style="background-image: url(../../data/img/texture_bg.webp);width:1080px;height:510px;padding:60px;display: flex;flex-direction: column;justify-content: space-between;"><div style="display: flex;flex-direction: row;justify-content: space-between;"><img src="../../data/img/index/hero_logo.png" style="width:540px;"/><div>${authorPortraits}</div></div><p style="font-family: IM Fell English;font-size: 64px;height: 245px;margin:0;">${title}</p></div></body>`;
+};
 
 async function writeCachedFile(CACHE_DIR, key, contents, extension) {
   const hash = createHash('md5')
@@ -20,9 +28,13 @@ async function writeCachedFile(CACHE_DIR, key, contents, extension) {
 }
 
 /*
- * Returns the path to an image generated from the provided HTML.
+ * Takes a post (probably a Gatsby node of some kind), generates some HTML,
+ * saves a screenshot, then returns the path to the saved image.
  */
-async function screenshotHTMLCard(CACHE_DIR, browser, title, html) {
+exports.postToImage = async (CACHE_DIR, browser, post) => {
+  const { title, author } = post.frontmatter;
+  const html = renderCard(title, author);
+
   const imageFileExtension = 'png';
 
   const filePath = await writeCachedFile(CACHE_DIR, title, html, 'html');
@@ -34,14 +46,4 @@ async function screenshotHTMLCard(CACHE_DIR, browser, title, html) {
   const file = await page.screenshot({ type: imageFileExtension });
 
   return writeCachedFile(CACHE_DIR, title, file, imageFileExtension);
-}
-
-/*
- * Takes a post (probably a Gatsby node of some kind), generates some HTML,
- * saves a screenshot, then returns the path to the saved image.
- */
-exports.postToImage = async (CACHE_DIR, browser, post) => {
-  const { title, author } = post.frontmatter;
-  const html = renderCard(title, author);
-  return screenshotHTMLCard(CACHE_DIR, browser, title, author, html);
 };
