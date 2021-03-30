9b82dc493276e43575977cf1a7c2606609e98b61
Sophie Au <SophieAu@users.noreply.github.com>
2021-03-30 22:48:21 +0200

    CORS in the version function

--- functions/version/version.js
+++ functions/version/version.js
@@ -1,6 +1,12 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+};
+
 const handler = async () => ({
   statusCode: 200,
+  headers,
   body: `2.0`,
 });
 
