c3fa194bbe816a6f29f96a87697f81aaf4379f4f
Sophie Au <SophieAu@users.noreply.github.com>
2021-06-23 22:15:55 +0200

    Make version function dynamic

--- functions/version/version.js
+++ functions/version/version.js
@@ -1,13 +1,30 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+//@ts-check
+
+require('isomorphic-fetch');
+
 const headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'Content-Type',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
 };
 
+const VERSION_SOURCE =
+  'https://raw.githubusercontent.com/SophieAu/imperial-splendour-launcher/master/version.txt';
+
+const getVersion = async () => {
+  try {
+    const response = await (await fetch(VERSION_SOURCE)).text();
+    return response.trim();
+  } catch (e) {
+    console.log({ e });
+  }
+};
+
 const handler = async () => ({
   statusCode: 200,
   headers,
-  body: `2.0`,
+  body: getVersion(),
 });
 
 module.exports = { handler };
