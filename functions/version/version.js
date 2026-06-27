243663d897a73c6eef9e9c887edd2d8c71094e8b
Sophie Au <SophieAu@users.noreply.github.com>
2026-06-27 23:18:55 +0200

    Actually allow the version error to throw by inlining the function

--- functions/version/version.js
+++ functions/version/version.js
@@ -12,21 +12,14 @@ const headers = {
 const VERSION_SOURCE =
   'https://raw.githubusercontent.com/SophieAu/imperial-splendour-launcher/master/version.txt';
 
-const getVersion = async () => {
-  try {
-    const response = await (await fetch(VERSION_SOURCE)).text();
-    return response.trim();
-  } catch (e) {
-    console.log({ e });
-  }
-};
-
 const handler = async () => {
   try {
-    const version = await getVersion()
+    const response = await (await fetch(VERSION_SOURCE)).text();
+    const version = response.trim()
     return { statusCode: 200, headers, body: version }
   }
   catch (e) {
+    console.log({ e });
     return { statusCode: 500, headers }
   }
 };
