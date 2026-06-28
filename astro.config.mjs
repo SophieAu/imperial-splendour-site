3dbae14fea3dc89f74149c445fbbeb56f94f04c3
Sophie Au <SophieAu@users.noreply.github.com>
2026-06-28 18:30:36 +0200

    Filter sitemap and use base_url in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,23 @@
-import svelte from '@astrojs/svelte';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-import { fileURLToPath } from 'url';
-import path from 'path';
+import svelte from "@astrojs/svelte";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import { fileURLToPath } from "url";
+import path from "path";
+import { BASE_URL, slugs } from "./src/config";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  site: 'https://imperialsplendour.com',
-  integrations: [svelte(), sitemap()],
+  site: BASE_URL,
+  integrations: [
+    svelte(),
+    sitemap({ filter: (page) => page !== `${BASE_URL}/${slugs.factions}` }),
+  ],
   vite: {
     resolve: {
       alias: {
-        '@assets': path.resolve(__dirname, 'assets'),
-        '@content': path.resolve(__dirname, 'content'),
+        "@assets": path.resolve(__dirname, "assets"),
+        "@content": path.resolve(__dirname, "content"),
       },
     },
   },
